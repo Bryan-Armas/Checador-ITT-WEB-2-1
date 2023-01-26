98f5da875a7748e538cd849868803ded06a9028c
Bryan-Armas <bryan.armas18@tectijuana.edu.mx>
2023-01-26 11:39:45 -0800

    update

--- Insertar.php
+++ Insertar.php
@@ -7,15 +7,16 @@
 <form action="cod_registro.php" method="post">
 <h2>Mi primer formulario del mini curso de php con Visual Studio Code </h2>
 <input type="number" id="IdEmpleados" name="IdEmpleados" require placeholder="NoControl"/>
-<input type="text" id="Nom" name="Nom" require placeholder="Nombre" Paterno»/>
-<input type="text" id="Ape" name="Ape" require placeholder="Apellido" Materno»/>
+<input type="text" id="Nom" name="Nom" require placeholder="Nombre"/>
+<input type="text" id="Ape" name="Ape" require placeholder="Apellido"/>
 <input type="number" id="Tel" name="Tel" require placeholder="Telefono"/>
 <button type="submit">Registrar usuario</button>
 </form>
 
 <!-- 
     posible link de conexion de base de datos:
-    https://www.youtube.com/watch?v=39b4vvfqL58&ab_channel=divcode
+
+    https://www.youtube.com/watch?v=sRuU9_5-sHs&ab_channel=yacklyon
 
 -->
 </body>
